c83d2c6afe2fc25505f84c47d55d0b51de2b45bc
Mauricio Brito <maubot01@gmail.com>
2020-11-21 08:35:31 -0400

    photo card component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { ListOfCategories } from './components/ListOfCategories/ListOfCategories.component'
 import { GlobalStyles } from './GlobalStyles'
+import {PhotoCard} from './components/PhotoCard/PhotoCard.component'
 
 export const App = () => 
   (<div>
     <GlobalStyles/>
     <ListOfCategories/>
+    <PhotoCard/>
   </div>)
