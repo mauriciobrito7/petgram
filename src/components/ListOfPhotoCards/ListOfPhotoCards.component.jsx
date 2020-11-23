d82193e41240e97bf2a077b6796e87b7c488870f
Mauricio Brito <maubot01@gmail.com>
2020-11-22 20:43:34 -0400

    persist likes in localStorage

--- src/components/ListOfPhotoCards/ListOfPhotoCards.component.jsx
+++ src/components/ListOfPhotoCards/ListOfPhotoCards.component.jsx
@@ -6,7 +6,7 @@ export const ListOfPhotoCard = () => {
         <div>
             <ul>
                 { [1,2,3,4,5,6,7].map(id => (
-                    <PhotoCard key={id}/>
+                    <PhotoCard key={id} id={id}/>
                 ))}
             </ul>
         </div>
