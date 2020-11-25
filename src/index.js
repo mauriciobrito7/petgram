34e2fe69e2d07618b94c47fd4e20a42b3025b138
Mauricio Brito <maubot01@gmail.com>
2020-11-24 23:12:21 -0400

    api

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
 import {App} from './App';
 import reportWebVitals from './reportWebVitals';
+
+const client = new ApolloClient({
+  uri:'https://petgram-server.midudev.now.sh/graphql'
+})
 
 ReactDOM.render(
   <React.StrictMode>
