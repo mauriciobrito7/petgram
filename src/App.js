01b66183cbaba831906c8dd0038f1d77051af8d3
Mauricio Brito <maubot01@gmail.com>
2020-11-27 23:01:59 -0400

    getting a single photo from graphql for details

--- src/App.js
+++ src/App.js
@@ -3,12 +3,25 @@ import { GlobalStyles } from './styles/GlobalStyles'
 import { ListOfCategories } from './components/ListOfCategories/ListOfCategories.component'
 import { ListOfPhotoCards } from './components/ListOfPhotoCards/ListOfPhotoCards.component'
 import { Logo } from './components/Logo/Logo.component'
+import { PhotoCardWithQuery } from './container/PhotoCardWithQuery'
 
 
-export const App = () => 
-  (<div>
+export const App = () => {
+  const urlParams = new window.URLSearchParams(window.location.search)
+
+  const detailId = urlParams.get('detail')
+
+  return (<div>
     <GlobalStyles/>
     <Logo/>
-    <ListOfCategories/>
-    <ListOfPhotoCards categoryId={1}/>
+    { 
+      detailId ? <PhotoCardWithQuery id={detailId} /> 
+      :
+      <>
+        <ListOfCategories/>
+        <ListOfPhotoCards categoryId={1}/>
+      </>
+    }
+    
   </div>)
+}
--- src/components/PhotoCard/PhotoCard.component.jsx
+++ src/components/PhotoCard/PhotoCard.component.jsx
@@ -3,21 +3,6 @@ import { ImgWrapper, Img, Button, Article } from './PhotoCard.styles'
 import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
 import { useLocalStorage } from '../../hooks/useLocalStorage.hook'
 import { useNearScreen } from '../../hooks/useNearScreen.hook'
-import { graphql } from 'react-apollo'
-import { gql } from 'apollo-boost'
-
-const withPhotos = graphql(gql`
-    query getPhotos {
-        photos {
-            id
-            categoryId
-            src
-            likes
-            userId
-            liked
-        }
-    }
-`)
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
@@ -33,7 +18,7 @@ export const PhotoCard = ({id, likes = 0, src = DEFAULT_IMAGE}) => {
         <Article ref={element}>
             {show && (
                 <>
-                    <a href={`/detail/${id}`}>
+                    <a href={`/?detail=${id}`}>
                     <ImgWrapper>
                         <Img src={src} />
                     </ImgWrapper>
