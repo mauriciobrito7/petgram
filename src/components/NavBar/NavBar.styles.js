e08a28ec675b6157856e70119e34e578fccb9ab2
Mauricio Brito <maubot01@gmail.com>
2020-12-06 17:56:15 -0400

    styling ative links

--- src/components/NavBar/NavBar.styles.js
+++ src/components/NavBar/NavBar.styles.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
 import { Link as LinkRouter } from '@reach/router'
+import { fadeIn } from '../../styles/animation'
 
 export const Nav = styled.nav`
   display:flex;
@@ -21,9 +22,20 @@ export const Link = styled(LinkRouter)`
   display: inline-flex;
   align-items: center;
   color: #888;
-  size: 32px;
   height: 100%;
   justify-content: center;
   text-decoration: none;
   width: 100%;
+  &[aria-current] {
+    color: #000;
+
+    &:after {
+      ${fadeIn({ time: '0.5s' })}
+      content: '-';
+      position:absolute;
+      bottom: 0;
+      font-size: 34px;
+      line-height: 20px;
+    }
+  }
 `
