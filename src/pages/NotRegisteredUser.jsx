18f342c143e4dca62ad2211c8c15c257924a90ea
Mauricio <maubot01@gmail.com>
2021-02-16 18:04:42 -0400

    new image

--- src/pages/NotRegisteredUser.jsx
+++ src/pages/NotRegisteredUser.jsx
@@ -1,48 +1,61 @@
-import React, { useContext } from 'react'
-import { Context } from '../Context'
-import { UserForm }  from '../components/UserForm/UserForm.component'
-import { RegisterMutation } from '../container/RegisterMutation'
-import { LoginMutation } from '../container/LoginMutation'
-import { Layout } from '../components/Layout/Layout.component'
+import React, { useContext } from "react";
+import { Context } from "../Context";
+import { UserForm } from "../components/UserForm/UserForm.component";
+import { RegisterMutation } from "../container/RegisterMutation";
+import { LoginMutation } from "../container/LoginMutation";
+import { Layout } from "../components/Layout/Layout.component";
 
 const NotRegisteredUser = () => {
-  const { activateAuth } = useContext(Context)
-  return <Layout title='Login'>
-  <RegisterMutation>
-    {
-      (register, { data, loading, error }) => {
-        const onSubmit = ({ email, password }) => {
-          const input  = { email, password }
-          const variables = { input }
-          register({ variables }).then(({data}) => {
-            const { signup } = data
-            activateAuth(signup)
-          })
-        }
-        const errorMsg = error && 'the user have already exist'
+  const { activateAuth } = useContext(Context);
+  return (
+    <Layout title="Login">
+      <RegisterMutation>
+        {(register, { data, loading, error }) => {
+          const onSubmit = ({ email, password }) => {
+            const input = { email, password };
+            const variables = { input };
+            register({ variables }).then(({ data }) => {
+              const { signup } = data;
+              activateAuth(signup);
+            });
+          };
+          const errorMsg = error && "the user have already exist";
 
-        return <UserForm disabled={loading} error={errorMsg} onSubmit={onSubmit} title='Registrarse' />
-      }
-    }
-  </RegisterMutation>
+          return (
+            <UserForm
+              disabled={loading}
+              error={errorMsg}
+              onSubmit={onSubmit}
+              title="Registrarse"
+            />
+          );
+        }}
+      </RegisterMutation>
 
-  <LoginMutation>
-    {
-      (login, { data, loading, error }) => {
-        const onSubmit = ({ email, password }) => {
-          const input  = { email, password }
-          const variables = { input }
-          login({ variables }).then(({data}) => {
-            const { login } = data
-            activateAuth(login)
-          })
-        }
-        const errorMsg = error && 'the password is wrong or the user does not exist' 
-        return  <UserForm disabled={loading} error={errorMsg} onSubmit={onSubmit} title='Iniciar sesion' />
-      }
-    }
-  </LoginMutation>
-</Layout>
-}
+      <LoginMutation>
+        {(login, { data, loading, error }) => {
+          const onSubmit = ({ email, password }) => {
+            const input = { email, password };
+            const variables = { input };
+            login({ variables }).then(({ data }) => {
+              const { login } = data;
+              activateAuth(login);
+            });
+          };
+          const errorMsg =
+            error && "the password is wrong or the user does not exist";
+          return (
+            <UserForm
+              disabled={loading}
+              error={errorMsg}
+              onSubmit={onSubmit}
+              title="Iniciar sesion"
+            />
+          );
+        }}
+      </LoginMutation>
+    </Layout>
+  );
+};
 
-export default NotRegisteredUser
+export default NotRegisteredUser;
