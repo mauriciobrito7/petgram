1d8c1f41fb479577fab84f6007ad3708c231304c
Mauricio <maubot01@gmail.com>
2021-02-15 23:47:06 -0400

    new color

--- src/components/FloatMessage/FloatMessage.styles.js
+++ src/components/FloatMessage/FloatMessage.styles.js
@@ -1,31 +1,34 @@
-import styled, { css } from 'styled-components'
-import {fadeIn} from '../../styles/animation'
+import styled, { css } from "styled-components";
+import { fadeIn } from "../../styles/animation";
+import { colors } from "../../styles/theme";
 
 export const ContainerMessage = styled.div`
-    width:50%;
-    height: 32px;
-    font-size:16px;
-    border-radius:1em;
-    background:white;
-    box-shadow: 0 0 20px rgba(0,0,0,0.2);
-    display: flex;
-    justify-content:center;
-    align-items:center;
-    position:fixed;
-    left: calc(50% - 25%);
-    bottom:calc(50px + 1em);
-    ${fadeIn({time:'0.3s'})}
-    ${props => props.type === 'alert' && css`
-        background:red;
-        color:white;
+  width: 50%;
+  height: 32px;
+  font-size: 16px;
+  border-radius: 1em;
+  background: ${colors.black};
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  left: calc(50% - 25%);
+  bottom: calc(50px + 1em);
+  ${fadeIn({ time: "0.3s" })}
+  ${(props) =>
+    props.type === "alert" &&
+    css`
+      background: red;
+      color: white;
     `}
     @media (min-width: 720px) {
-        width:25%;
-        left: calc(50% - 12%);
-    }
+    width: 25%;
+    left: calc(50% - 12%);
+  }
 
-    @media (min-width: 1280px) {
-        width:15%;
-        left: calc(50% - 7%);
-    }
-`
+  @media (min-width: 1280px) {
+    width: 15%;
+    left: calc(50% - 7%);
+  }
+`;
