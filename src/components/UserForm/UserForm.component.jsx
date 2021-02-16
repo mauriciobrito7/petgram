35629cee46173b384c90b780840cee0ef162aae0
Mauricio <maubot01@gmail.com>
2021-02-16 18:07:53 -0400

    new styles

--- src/components/UserForm/UserForm.component.jsx
+++ src/components/UserForm/UserForm.component.jsx
@@ -1,41 +1,48 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-import { useInputValue } from '../../hooks/useInputValue.hook'
-import { Form, Input, Title, Error } from './UserForm.styles'
-import { SubmitButton } from '../SubmitButton/SubmitButton.componet'
+import { useInputValue } from "../../hooks/useInputValue.hook";
+import { Form, Input, Title, Error } from "./UserForm.styles";
+import { SubmitButton } from "../SubmitButton/SubmitButton.componet";
 
 export const UserForm = ({ onSubmit, title, error = null, disabled }) => {
-
-  const email = useInputValue('')
-  const password = useInputValue('')
-  const [errorMsg, setErrorMsg] = useState(error)
+  const email = useInputValue("");
+  const password = useInputValue("");
+  const [errorMsg, setErrorMsg] = useState(error);
 
   const handleSubmit = (event) => {
-    event.preventDefault()
+    event.preventDefault();
 
-    if(email.value.length !== 0 && password.value.length) {
-      onSubmit({ 
-        email: email.value, 
-        password: password.value
-      })
-    }else{
-      setErrorMsg('Complete the form')
+    if (email.value.length !== 0 && password.value.length) {
+      onSubmit({
+        email: email.value,
+        password: password.value,
+      });
+    } else {
+      setErrorMsg("Complete the form");
     }
-  }
+  };
 
-  return  (
-  <>
-    <Form disabled={disabled} onSubmit={handleSubmit}> 
-      <Title>{title}</Title>
-      <Input disabled={disabled} placeholder='Email' {...email} type="email"/>
-      <Input disabled={disabled} placeholder='Password' {...password}  type="password"/>
+  return (
+    <>
+      <Form disabled={disabled} onSubmit={handleSubmit}>
+        <Title>{title}</Title>
+        <Input
+          disabled={disabled}
+          placeholder="Email"
+          {...email}
+          type="email"
+        />
+        <Input
+          disabled={disabled}
+          placeholder="Password"
+          {...password}
+          type="password"
+        />
 
-      <SubmitButton disabled={disabled}>{title}</SubmitButton>
-    </Form>
+        <SubmitButton disabled={disabled}>{title}</SubmitButton>
+      </Form>
 
-    {errorMsg &&
-      <Error>{errorMsg}</Error>
-    }
-  </>
-  )
-}
+      {errorMsg && <Error>{errorMsg}</Error>}
+    </>
+  );
+};
--- src/components/UserForm/UserForm.styles.js
+++ src/components/UserForm/UserForm.styles.js
@@ -1,26 +1,38 @@
-import styled from 'styled-components'
+import styled from "styled-components";
+import { colors } from "../../styles/theme";
 
 export const Form = styled.form`
   padding: 16px 0;
-`
-
+`;
 
 export const Input = styled.input`
   border: 1px solid #ccc;
+  background: ${colors.darkGray};
+  color: ${colors.white};
   border-radius: 3px;
   margin-bottom: 8px;
-  padding: 8px 4px;
+  padding: 1em 1em;
   display: block;
   width: 100%;
-`
+  transition: background 0.3s ease-in-out;
+  &:focus {
+    outline: none;
+  }
+  &::placeholder {
+    color: ${colors.white};
+  }
+  &:hover {
+    background: ${colors.lightGray};
+  }
+`;
 
 export const Title = styled.h2`
-  font-size:16px;
-  font-weight: 500;
+  font-size: 16px;
+  font-weight: 600;
   padding: 8px 0;
-`
+`;
 
 export const Error = styled.span`
   color: red;
   font-size: 14px;
-`
+`;
