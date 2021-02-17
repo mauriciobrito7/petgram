1d6ce721c1978a6e227ecf32e7867352a6230057
Mauricio <maubot01@gmail.com>
2021-02-16 21:11:59 -0400

    😅

--- src/index.js
+++ src/index.js
@@ -1,42 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ApolloClient from 'apollo-boost'
-import { ApolloProvider } from 'react-apollo'
-import {App} from './App';
-import reportWebVitals from './reportWebVitals';
-import {Provider} from './Context'
-import { BrowserRouter } from 'react-router-dom'
-
+import React from "react";
+import ReactDOM from "react-dom";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import { App } from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "./Context";
+import { BrowserRouter } from "react-router-dom";
 
 const client = new ApolloClient({
-  uri:'https://petgram-server-23xyhmeq5.vercel.app/graphql',
-  request: operation => {
-    const token = window.sessionStorage.getItem('token')
-    const authorization = token ? `Bearer ${token}` : ''
+  uri: "https://petgram-server-23xyhmeq5.vercel.app/graphql",
+  request: (operation) => {
+    const token = window.sessionStorage.getItem("token");
+    const authorization = token ? `Bearer ${token}` : "";
     operation.setContext({
       headers: {
-        authorization
-      }
-    })
+        authorization,
+      },
+    });
   },
-  onError: error => {
-    const { networkError } = error
-    if(networkError && networkError.result.code === 'invalid_token') {
-      window.sessionStorage.removeItem('token')
-      window.location.href = '/'
+  onError: (error) => {
+    const { networkError } = error;
+    if (networkError && networkError.result.code === "invalid_token") {
+      window.sessionStorage.removeItem("token");
+      window.location.href = "/";
     }
-  }
-})
+  },
+});
 
 ReactDOM.render(
-    <Provider>
-      <BrowserRouter>
-        <ApolloProvider client={client}>
-          <App />
-        </ApolloProvider>
-      </BrowserRouter>
-    </Provider>,
-  document.getElementById('root')
+  <Provider>
+    <BrowserRouter>
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
